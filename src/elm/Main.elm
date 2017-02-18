b44d89f6f9fa9929f32202694a1887366f8fd600
Robert Bethencourt <robert.bethencourt@gmail.com>
2017-02-18 11:46:13 -0500

    elm webpack starter initial site files

---
+++ src/elm/Main.elm
@@ -0,0 +1,110 @@
+-- module Main exposing (..)
+--
+-- import Html exposing (..)
+-- import Html.Events exposing (..)
+-- import Html.Attributes exposing (..)
+--
+--
+-- -- model
+--
+--
+-- type alias Model =
+--     { titleInput : String
+--     , doodles : List Doodle
+--     , error : Maybe String
+--     }
+--
+--
+-- type alias Doodle =
+--     { title : String
+--     , doodle : String
+--     }
+--
+--
+-- tempDoodles : List Doodle
+-- tempDoodles =
+--     [ Doodle "title number 1" "doodle number 1"
+--     , Doodle "22222" "another doodle"
+--     ]
+--
+--
+-- initModel : Model
+-- initModel =
+--     { titleInput = ""
+--     , doodles = tempDoodles
+--     , error = Nothing
+--     }
+--
+--
+-- type alias Flags =
+--     { error : Maybe String }
+--
+--
+-- init : Flags -> ( Model, Cmd Msg )
+-- init flags =
+--     ( initModel, Cmd.none )
+--
+--
+--
+-- -- update
+--
+--
+-- type Msg
+--     = TitleInput String
+--     | Error String
+--
+--
+-- update : Msg -> Model -> ( Model, Cmd Msg )
+-- update msg model =
+--     case msg of
+--         TitleInput titleInput ->
+--             ( { model | titleInput = titleInput }, Cmd.none )
+--
+--         Error error ->
+--             ( { model | error = Just error }, Cmd.none )
+--
+--
+--
+-- -- view
+--
+--
+-- view : Model -> Html Msg
+-- view model =
+--     div []
+--         [ navBar
+--         , div []
+--             [ input [ type_ "text", value model.titleInput, onInput TitleInput ] []
+--             , p [] [ text (toString model) ]
+--             ]
+--         ]
+--
+--
+-- navBar : Html Msg
+-- navBar =
+--     header []
+--         [ nav [ class "navbar" ]
+--             [ div [ class "container-fluid" ]
+--                 [ div [ class "navbar-header" ]
+--                     [ a [ class "navbar-brand", href "#" ] [ text "Picasso" ] ]
+--                 ]
+--             ]
+--         ]
+--
+--
+--
+-- -- subscriptions
+--
+--
+-- subscriptions : Model -> Sub Msg
+-- subscriptions model =
+--     Sub.none
+--
+--
+-- main : Program Flags Model Msg
+-- main =
+--     programWithFlags
+--         { init = init
+--         , update = update
+--         , view = view
+--         , subscriptions = subscriptions
+--         }
