62a0a676de993b74b53adf14ce70cfbb3f6e71cb
Robert Bethencourt <robert.bethencourt@gmail.com>
2017-02-24 15:29:44 -0500

    more server changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static('./dist'))
 
 // Main Route. This route will redirect to our rendered React application
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + './index.html')
+  res.sendFile(__dirname + './dist/index.html')
 }) // end app.get()
 
 // this is the listener setup for integrating socket.io
